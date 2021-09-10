5f9e67db4c050f032c85c8984ed47f7e9dedf28b
SJRXD <rohr.olasz@gmail.com>
2021-09-09 18:46:13 -0700

    attempting to fix my promise issues

--- db/employeeBuilds.js
+++ db/employeeBuilds.js
@@ -1,24 +1,23 @@
 const connection = require("../config/connection");
 
 class employeeDB {
-
     constructor(connection) {
         this.connection = connection;
     }
 
-    // Show all employees
+    // SHOW ALL EMPLOYEES // 
     allEmployees() {
-        return this.connection.promise().query(
+        return this.connection.query(
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
     }
 
-    // Add an employee
+    // ADD EMPLOYEE //
     addEmployee(employee) {
         return this.connection.promise().query("INSERT INTO employee SET ?", employee);
     }
 
-    // Update the given employee's role
+    // UPDATE EMPLOYEE ROLE //
     updateEmployeeRole(employeeId, roleId) {
         return this.connection.promise().query(
             "UPDATE employee SET role_id = ? WHERE id = ?",
@@ -27,7 +26,7 @@ class employeeDB {
     }
 
 
-    // Show all managers
+    // SHOW MANAGERS //
     allManagers(employeeId) {
         return this.connection.promise().query(
             "SELECT id, first_name, last_name FROM employee WHERE id != ?",
@@ -35,30 +34,29 @@ class employeeDB {
         );
     }
 
-    // Show all roles
+    // SHOW ROLES //
     allRoles() {
         return this.connection.promise().query(
             "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
         );
     }
 
-    // Create a new role
+    // CREATE ROLE //
     addRole(role) {
         return this.connection.promise().query("INSERT INTO role SET ?", role);
     }
 
-    // Show all departments
+    // SHOW DEPARTMENTS //
     allDepartments() {
         return this.connection.promise().query(
             "SELECT department.id, department.name FROM department;"
         );
     }
 
-    // Add a department
+    // ADD A DEPARTMENT //
     addDepartment(department) {
         return this.connection.promise().query("INSERT INTO department SET ?", department);
     }
-
 }
 
 module.exports = new employeeDB(connection);
