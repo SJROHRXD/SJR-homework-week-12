d63df25598250ced23ee99a1015255cfe77caeea
SJRXD <rohr.olasz@gmail.com>
2021-12-06 20:53:59 -0800

    fixed promise for all employees

--- db/employeeBuilds.js
+++ db/employeeBuilds.js
@@ -1,15 +1,21 @@
 const connection = require("../config/connection");
 
 class employeeDB {
-    constructor(connection) {
+    constructor( connection ) {
         this.connection = connection;
     }
 
-    // SHOW ALL EMPLOYEES // 
+    // VIEW ALL EMPLOYEES //
     allEmployees() {
-        return this.connection.query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-        );
+        return new Promise((resolve, reject) => {
+            this.connection.query(
+              "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+              function (err, results) {
+                resolve(results);
+                reject(err);
+              }
+            );
+          });
     }
 
     // ADD EMPLOYEE //
@@ -59,4 +65,31 @@ class employeeDB {
     }
 }
 
-module.exports = new employeeDB(connection);
+// module.exports = new employeeDB(connection);
+
+
+//////
+const mysql = require( 'mysql' );
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
